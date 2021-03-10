4ac714fbf99260c1cd4c5eff7a58e14533f834bd
metamagicalfun <none@none.com>
2021-03-10 13:35:27 -0600

    going to learn about tcp!

--- main.cpp
+++ main.cpp
@@ -1,12 +1,61 @@
+#include <iostream>
+#include <string>
+
 #include "crypto/md5.h"
+
+
+class PacketHeader
+{
+  public:
+    PacketHeader(int seq, int ack, int syn) :
+        seq_(seq)
+       ,syn_(ack)
+       ,ack_(syn)
+    {
+    }	
+
+
+ private:
+    int seq_;
+    int syn_;
+    int ack_;
+};
+
+
+class Packet
+{
+  public:
+    Packet(PacketHeader packetHeader, std::string filename)
+		: packetHeader_(packetHeader)
+		, filename_(filename)
+	{		        
+    	data_ = {md5_from_file(filename_)};
+	}
+	
+	bool is_valid(std::string& hash) {
+		return hash == data_;
+	}
+
+
+  private:
+	PacketHeader packetHeader_;
+	std::string filename_;
+	std::string data_;
+};
+
 
 
 int main(void)
 {
-    std::string md5{md5_from_file("test_file.txt")};
+    PacketHeader header{1,2,3};
+    Packet packet{header, "test_file.txt"};
+	
+	std::string md5 = "100e0ac23e8e798f1ac649a2303f1076";
 
-    bool match = md5 == "100e0ac23e8e798f1ac649a2303f1076" ;
+	bool result = packet.is_valid(md5);
 
-    return match;
+    std::cout << result << std::endl;
+
+    return 0;
 }
 
